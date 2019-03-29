a583b0c27597c5b7529e6de8c2a4c130a57ac3b1
Chetan Shriram Umale <csu1@cs1-15-21.local>
2019-03-29 19:13:24 +0000

    Initial Commit

---
+++ Indian Cricket Team/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Indian Cricket Team
+//
+//  Created by Chetan Shriram Umale on 29/03/2019.
+//  Copyright © 2019 Chetan Shriram Umale. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
